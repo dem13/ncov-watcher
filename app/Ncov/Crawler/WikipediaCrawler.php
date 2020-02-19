17870aaf7215a361b619009fb63d073e9e2d3c56
demyansson <idcadifi@gmail.com>
2020-02-19 23:48:31 +0200

    fix wikipedia crawler

--- app/Ncov/Crawler/WikipediaCrawler.php
+++ app/Ncov/Crawler/WikipediaCrawler.php
@@ -8,12 +8,12 @@ class WikipediaCrawler implements ICrawler
 {
     private $client;
 
-    private $uri = 'https://ru.wikipedia.org/wiki/%D0%92%D1%81%D0%BF%D1%8B%D1%88%D0%BA%D0%B0_%D0%BA%D0%BE%D1%80%D0%BE%D0%BD%D0%B0%D0%B2%D0%B8%D1%80%D1%83%D1%81%D0%B0_2019-nCoV';
+    private $uri = 'https://ru.wikipedia.org/wiki/%D0%92%D1%81%D0%BF%D1%8B%D1%88%D0%BA%D0%B0_COVID-19';
 
     private $xpathQueries = [
-        'deaths' => '//*[@id="mw-content-text"]/div/table[4]/tbody/tr[last()]/td[4]',
-        'infected' => '//*[@id="mw-content-text"]/div/table[4]/tbody/tr[last()]/td[2]',
-        'cured' => '//*[@id="mw-content-text"]/div/table[4]/tbody/tr[last()]/td[5]',
+        'deaths' => '//*[@id="mw-content-text"]/div/table[3]/tbody/tr[last()]/td[4]',
+        'infected' => '//*[@id="mw-content-text"]/div/table[3]/tbody/tr[last()]/td[2]',
+        'cured' => '//*[@id="mw-content-text"]/div/table[3]/tbody/tr[last()]/td[5]',
     ];
 
     public function __construct()
@@ -62,7 +62,7 @@ class WikipediaCrawler implements ICrawler
         $result = [];
 
         foreach ($this->xpathQueries as $key => $query) {
-            $result[$key] = $xpath->query($query)[0]->textContent ?? null;
+            $result[$key] = str_replace(' ', '', $xpath->query($query)[0]->textContent);
         }
         return $result;
     }
