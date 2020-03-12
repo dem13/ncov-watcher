74e80419b5cccce5fa3f281df6ed128aa8f96832
demyansson <idcadifi@gmail.com>
2020-03-12 12:14:19 +0200

    telegram bot subscribers message sending

---
+++ app/Services/TelegramService.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Services;
+
+use App\Telegram\Repositories\ChatRepository;
+use Illuminate\Contracts\Filesystem\Filesystem;
+use Telegram\Bot\Api;
+
+class TelegramService
+{
+    /**
+     * @var Api
+     */
+    private $telegram;
+
+    /**
+     * @var ChatRepository
+     */
+    private $chatRepo;
+
+    /**
+     * @var Filesystem
+     */
+    private $storage;
+
+    public function __construct(Api $telegram, ChatRepository $chatRepo, Filesystem $storage)
+    {
+        $this->telegram = $telegram;
+        $this->chatRepo = $chatRepo;
+        $this->storage = $storage;
+    }
+
+    /**
+     * Send message to all subscribed chats
+     *
+     * @param $message
+     * @param array $params
+     * @return array
+     */
+    public function sendMessageToSubscribers($message, $params = []): array
+    {
+        if (empty($message)) {
+            return [];
+        }
+
+        $params['text'] = $message;
+
+        return $this->sendForEachSubscriber(fn ($params) => $this->telegram->sendMessage($params), $params);
+    }
+
+    /**
+     * Send telegram message to all subscribers
+     *
+     * @param \Closure $callback
+     * @param array $params
+     * @return array
+     */
+    private function sendForEachSubscriber(\Closure $callback, $params = []): array
+    {
+        $chats = $this->chatRepo->getSubsbcribed();
+
+        $sent = 0;
+        $failed = 0;
+
+
+        foreach ($chats as $chat) {
+            try {
+                $params['chat_id'] = $chat->id;
+
+                $callback($params);
+
+                $sent++;
+            } catch (\Exception $e) {
+                $failed++;
+            }
+        }
+
+        return [
+            'total' => $chats->count(),
+            'sent' => $sent,
+            'failed' => $failed,
+        ];
+    }
+
+    /**
+     * Send photo to all subscribers
+     *
+     * @param $path
+     * @param array $params
+     * @return array
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    public function sendPhotoToSubscribers($path, $params = []): array
+    {
+        if (!$this->storage->exists($path)) {
+            return [];
+        }
+
+        $params['photo'] = $this->storage->readStream($path);
+
+        return $this->sendForEachSubscriber(fn ($params) => $this->telegram->sendPhoto($params), $params);
+    }
+}
--- app/Telegram/Repositories/ChatRepository.php
+++ app/Telegram/Repositories/ChatRepository.php
@@ -3,6 +3,7 @@
 namespace App\Telegram\Repositories;
 
 use App\Telegram\Models\Chat;
+use Illuminate\Support\Collection;
 
 class ChatRepository
 {
@@ -48,5 +49,15 @@ class ChatRepository
     public function find($id): ?Chat
     {
         return Chat::find($id);
+    }
+
+    /**
+     * Get subscribed chats
+     *
+     * @return Collection
+     */
+    public function getSubsbcribed(): Collection
+    {
+        return Chat::where('subscribed', true)->get();
     }
 }
